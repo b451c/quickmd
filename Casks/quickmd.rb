64e8820978504017fe09dc701c3fe0d51990bc91
Bartosz Sroczynski <b.sroczynski@pm.me>
2026-08-17 16:23:00 +0200

    chore: sync local Casks/quickmd.rb to v1.9.0

--- Casks/quickmd.rb
+++ Casks/quickmd.rb
@@ -1,6 +1,6 @@
 cask "quickmd" do
-  version "1.8.0"
-  sha256 "5531494f5736610038ab7f65ff65bfe0060bb153842091a599a9b69c911ddb3c"
+  version "1.9.0"
+  sha256 "748ee2356f9829c2ea7037b16fdae7a1496f85e9ff84843fa6ebc1f43a677ad6"
 
   url "https://github.com/b451c/quickmd/releases/download/v#{version}/QuickMD-v#{version}.zip",
       verified: "github.com/b451c/quickmd/"
